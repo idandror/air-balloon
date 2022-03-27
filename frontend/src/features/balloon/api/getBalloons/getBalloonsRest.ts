882216d376b0129c23b31dd4bbc22145ac661945
idandror <eidan.dror@devalore.com>
2022-03-27 09:25:29 +0300

    clean

--- frontend/src/features/balloon/api/getBalloons/getBalloonsRest.ts
+++ frontend/src/features/balloon/api/getBalloons/getBalloonsRest.ts
@@ -1,19 +1,19 @@
-import axios from "axios";
-import { isMock } from "../../../../utils/constants";
-import { mockGetBalloons } from "../mockBalloon";
-import { API_URL } from "../../reduxSlice/balloonSlice";
+import axios from 'axios';
+import { isMock } from '../../../../utils/constants';
+import { mockGetBalloons } from '../mockBalloon';
+import { API_URL } from '../../reduxSlice/balloonSlice';
 
 export const getBalloonsRest = async (token: String) => {
-	if (isMock) {
-	  mockGetBalloons(token);
-	}
-	const config = {
-	  headers: {
-		Authorization: `Bearer ${token}`,
-	  },
-	};
-  
-	const response = await axios.get(API_URL + '/getAll', config);
-  
-	return response.data;
+  if (isMock) {
+    mockGetBalloons(token);
+  }
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   };
+
+  const response = await axios.get(API_URL + '/getAll', config);
+
+  return response.data;
+};
